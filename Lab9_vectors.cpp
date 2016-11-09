fd1790713d6460044a4c8845a8534a84263bf888
jakebracey <jakebracey62@gmail.com>
2016-11-08 21:54:25 -0600

    finished vectors

--- Lab9_vectors.cpp
+++ Lab9_vectors.cpp
@@ -46,7 +46,60 @@ int main()
 	ivec2.push_back(50);
 	cout << "New size: " << ivec2.size() << "\nAdded element: " << ivec2[0] << endl;
 	cout << "\n------------------------------------------------------------------" << endl;
-		
+
+	//initializing vectors all the ways shown in Table 3.4
+	vector<double> v1;
+	vector<double> v2(dvec1);
+	vector<double> v3=v2;
+	vector<float> v4(3,2.3);
+	vector<double> v5(2);
+	vector<float> v6{3,5,87,2.7,3};
+	vector<int> v7={3,5,87,2,3};
+	
+	//Trying all the vector operations shown in Table 3.5
+	cout<<endl<<"Is the vector v1 empty? "<<v1.empty();
+	cout<<endl<<"Is the vector v2 empty? "<<v2.empty();
+	cout<<endl<<"Number of elements in v4 is: "<<v4.size();
+	v4.push_back(5);
+	cout<<endl<<"Number of elements in v4 after the push_back is: "<<v4.size();
+	cout<<endl<<"The number that is at position 4 of v4 is: "<<v4[3];
+	v1=v2;
+	cout<<endl<<"The empty vector v1 now has the value of: "<<v1[0];
+	v3={5.4,3.2,5.3};
+	cout<<endl<<"Vector v3 now contains the following values:"<<endl;
+	for(auto i:v3)
+		cout << i << endl;
+	cout<<endl<<"Does v1 have the same number of elements as v2? "<<(v1 == v2);
+	cout<<endl<<"Does v1 not have the same number of elements as v2? "<<(v1 != v2);
+	
+	
+	
+	//exercise 3.14 read a sequence of ints from cin into a vector
+	int numb[5];//array to hold my input numbers
+	
+	for(int i=0;i<5;i++){
+		cout<<endl<<"Enter a number of integer type: ";
+		cin>>numb[i];
+	}
+	vector<int> v8{numb[0],numb[1],numb[2],numb[3],numb[4]};
+	
+	for(auto i:v8)
+		cout << i << endl;
+	cout << "\n------------------------------------------------------------------" << endl;
+
+	//exercise 3.15 read a sequence of strings from cin into a vector
+	string str1[5];//array to hold my input numbers
+	
+	for(int i=0;i<5;i++){
+		cout<<endl<<"Enter a string: ";
+		cin>>str1[i];
+	}
+	vector<string> v9{str1[0],str1[1],str1[2],str1[3],str1[4]};
+	
+	for(auto i:v9)
+		cout << i << endl;
+	cout << "\n------------------------------------------------------------------" << endl;
+	
 // ***********************************************************************
 // Try all the wasy to initializa a vector shown in Table 3.4. Use the
 // vectors above and/or declare new vectors.
